c696dc00ac54be0fa0896bcbe90f36121b095afc
Steve Yardumian <steveyardumian@gmail.com>
2020-04-22 13:10:59 -0400

    First commit

---
+++ __tests__/particle.spec.js
@@ -0,0 +1,46 @@
+import AWS from 'aws-sdk'
+import { particlePublishEvent } from '../src/particle'
+
+const AWSMock = require('aws-sdk-mock')
+AWSMock.setSDKInstance(AWS);
+
+describe('Particle Publish Event', () => {
+    beforeAll(() => {
+        AWSMock.mock('SecretsManager', 'getSecretValue', (params, callback) => {
+            callback(null, {"SecretString": '{"username": "****", "password": "****"}'})
+        })
+    })
+
+    afterAll(() => {
+        AWSMock.restore()
+    })
+
+    test('turn light on', (done) => {
+        return particlePublishEvent({
+                "body": "{\"action\":\"switchLightsState\",\"params\":\"on,livingroom,1\", \"secretId\": \"none\"}"
+            }, {}, (msg) => {
+                done(msg)
+            }).then((data) => {
+                expect(data.statusCode).toEqual(200)
+                done()
+            })
+    })
+
+    test('bad username and password', (done) => {
+        AWSMock.remock('SecretsManager', 'getSecretValue', (params, callback) => {
+            callback(null, {"SecretString": '{"username": "blah@gmail.com", "password": "none"}'})
+        })
+        return particlePublishEvent({
+                "body": "{\"action\":\"switchLightsState\",\"params\":\"on,livingroom,1\", \"secretId\": \"none\"}"
+            }, {}, (msg) => {
+                done(msg)
+            }).then((data) => {
+                expect(data.statusCode).toEqual(400)
+                done()
+            })
+    })
+
+    //TODO: need to add two more cases
+    //1)Failure to publish the Particle event
+    //2)Exception thrown during the try block
+})
---
+++ src/particle.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const Particle = require('particle-api-js')
+const particle = new Particle() 
+
+const AWS = require('aws-sdk')
+
+module.exports.particlePublishEvent = async (event, context, callback) => {
+
+	const sm = new AWS.SecretsManager()
+    
+    try{
+		let bodyJson = JSON.parse(event.body)
+		
+		let authData = await sm.getSecretValue({ SecretId: bodyJson.secretId }).promise()
+		let secretStr = JSON.parse(authData.SecretString)
+
+		return await particle.login({username: secretStr.username, password: secretStr.password})
+			.then(async (data) => {
+				let token = data.body.access_token
+				
+				return await particle.publishEvent({ name: bodyJson.action, data: bodyJson.params, auth: token })
+					.then((data) => {
+						return {
+							"statusCode": 200,
+							headers: {
+								'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+								'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+							},
+							"body": "Particle event published succesfully!"
+						}
+					},
+					(err) => {
+						return {
+							statusCode: 400,
+							body: JSON.stringify(err),
+						}
+					})
+			},
+			(err) => {
+				return {
+					statusCode: 400,
+					body: JSON.stringify(err),
+				}
+			})
+        
+    }catch(error) {
+        return {
+			statusCode: 400,
+			body: JSON.stringify(error),
+		}
+    }
+}
